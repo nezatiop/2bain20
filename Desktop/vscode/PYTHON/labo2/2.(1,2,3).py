50c5e198db544619d6a7c2c4ac028b46b72519f2
nezatiop <155076595+nezatiop@users.noreply.github.com>
2024-10-07 09:11:25 +0200

    Update 2.(1,2,3).py

--- Desktop/vscode/PYTHON/labo2/2.(1,2,3).py
+++ Desktop/vscode/PYTHON/labo2/2.(1,2,3).py
@@ -1,3 +1,4 @@
+#2.1
 from dataclasses import dataclass
 
 @dataclass
@@ -5,15 +6,18 @@ class Item:
     code : int
     name : str
     cents : int
-    
+
+    #2.2
     def unitprice(self):
         return self.cents / 100.0
     
     def price(self, quantity : int):
         return self.cents * quantity / 100.0
     
+    #2.3
     def __str__(self):
         return f"{self.name} ({self.code}, {self.unitprice()}€)"
+        
 
 beer = Item(56273, "Zundert 33 cl", 250)
 print(beer)
